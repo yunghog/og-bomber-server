2a48f6b00888388b4767e6a1365d1f441be373e1
Samartha <41548444+yunghog@users.noreply.github.com>
2020-04-21 22:25:25 +0530

    init

--- register.php
+++ register.php
@@ -4,13 +4,10 @@ $username=$_GET['un'];
 $os=$_GET['os'];
 $role=1;
 $query="INSERT INTO user(`username`, `os`, `role`) VALUES ('$username','$os',1)";
-echo $query;
 if($exec=mysqli_query($conn,$query)){
-  $data = "Status:Registration Successfully";
+  echo "success";
 }
 else {
-  $data="Status:Failed registration";
+  echo "failed";
 }
-header('Content-type: application/json');
-echo json_encode( $data );
 ?>
