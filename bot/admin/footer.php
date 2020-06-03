d7e11c33b09a3774d8f25dd7e5dbfd48a03f8231
Samartha <41548444+yunghog@users.noreply.github.com>
2020-06-03 22:09:10 +0530

    footer

---
+++ bot/admin/footer.php
@@ -0,0 +1,9 @@
+<footer>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12 text-center">
+      <p>coded with <i class="fa fa-flash active2"></i> by samartha</p>
+    </div>
+  </div>
+</div>
+</footer>
