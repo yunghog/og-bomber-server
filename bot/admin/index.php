633bcfabbbb68d950b1b666e82dda2fcf1e48267
Samartha <41548444+yunghog@users.noreply.github.com>
2020-06-03 21:55:06 +0530

    admin panel

--- bot/admin/index.php
+++ bot/admin/index.php
@@ -17,10 +17,12 @@ if(isset($_SESSION['user_id'])){
   </head>
   <body>
     <?php include 'header.php'; ?>
-
+    <script type="text/javascript">
+      document.getElementById('index').classList.add('active2');
+    </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="../js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
 <?php
