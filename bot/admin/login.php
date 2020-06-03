fd47aabd59ecbf0733b21644c8078e25b4dd06be
Samartha <41548444+yunghog@users.noreply.github.com>
2020-06-03 16:00:44 +0530

    login page

--- bot/admin/login.php
+++ bot/admin/login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>@keaton_bot</title>
+    <title>@keaton_bot | login</title>
     <meta name="author" content="Samartha">
     <meta name="description" content="spam message service byb Samartha">
     <link rel="icon" href="../images/favicon.png" sizes="16x16">
@@ -11,7 +11,22 @@
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-    
+    <section class="full-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
+            <div class="form-con">
+              <form class="form-control" action="index.html" method="post">
+                <div class="row">
+                  <p>Enter Passcode</p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
